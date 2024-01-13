c196fd959dee78b0c688d679f0105806d05bf672
Arvie420X <arvidaslobaton420x@gmail.com>
2024-01-13 21:32:38 +0800

    first commit

---
+++ litecart/dashboard/views.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+from  item.models import Item
+
+# Create your views here.
+def index(request):
+    items = Item.objects.filter(created_by=request.user)
+    
+    return render(request, 'dashboard/index.html', {
+        'items': items,
+    })
